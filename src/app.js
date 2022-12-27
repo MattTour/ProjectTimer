4257505097c65fc0bdce327bc6618784199265f1
MattTour <matthieu.tourneur7724@gmail.com>
2022-12-27 23:52:03 +0100

    Arbo front + module ejs pour html

--- src/app.js
+++ src/app.js
@@ -8,6 +8,10 @@ const port = 3000;
 
 const server = express();
 
+server.set('view engine', 'html');
+
+server.engine('html', require('ejs').renderFile);
+
 server.use(cors());
 const mongoose = require("mongoose");
 // mongoose.connect("mongodb://localhost:27017/apinode"); // Whithout Docker
@@ -27,5 +31,9 @@ taskRoute(server);
 
 const userRoute = require("./api/routes/userRoute");
 userRoute(server);
+
+server.get('/login', function(req, res){
+    res.render(__dirname + '/front/pages/login.html');
+});
 
 server.listen(port, hostname);
