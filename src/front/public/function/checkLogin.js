4d4d59d9fd865352bc0ff6671ed8369a5934c403
MattTour <matthieu.tourneur7724@gmail.com>
2022-12-29 12:08:30 +0100

    Listing des groupes sur la page d'accueil fonctionnel. Boutons delete et edit en place.

--- src/front/public/function/checkLogin.js
+++ src/front/public/function/checkLogin.js
@@ -1,7 +1,5 @@
-console.log("test");
 window.onload = function isLoggedIn () {
     const token = localStorage.getItem('token');
-    console.log(token);
     if (!token) {
         window.location.href = "/login";
     }
